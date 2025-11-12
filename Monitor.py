80240019452ab8c411bacffc6aaeee1325d72073
jwhittingham-code <jwhittingham002@gmail.com>
2025-11-12 22:28:31 +1100

    Added Ram usage and polished visuals

--- Monitor.py
+++ Monitor.py
@@ -3,21 +3,35 @@ import psutil
 import time
 
 
+
 while True:
     
     cpuCount = psutil.cpu_count(logical=True)
     cpuPercent = psutil.cpu_percent(interval=0.1, percpu= True)
     coretemp = psutil.sensors_temperatures()['coretemp']
-    #print(f"cpuCount: {cpuCount}\n cpuPercent: {cpuPercent} \n temp: {coretemp}")
-    
-    os.system('clear')
+    #=======================================================================
+    ram = psutil.virtual_memory()
+    ramTotal = round(ram.total/1024/1024/1024,2)
+    ramUsed = round(ram.used/1024/1024/1024,2)
+    ramFree = round(ram.available/1024/1024/1024,2) 
 
+    os.system('clear')
+    print("==================")
+    print("| System Monitor |")
+    print("==================") 
+    print("CPU Usage\n")  
     for i in range(cpuCount+1):
         core = coretemp[i][0]
         currenttemp = coretemp[i][1]
         maxtemp = coretemp[i][2]
         coreusage = cpuPercent[i-1]
         if core != 'Package id 0':
-            print(f"cpu: {core} \n{coreusage}%\nCurrent temp: {currenttemp} | Max temp: {maxtemp}\n")
-    
+            print(f"{core}:\t {coreusage}%\nCurrent temp: {currenttemp} | Max temp: {maxtemp}\n")
+        else:
+            print(f"TotalUsage: {coreusage}%\nCurrent temp: {currenttemp}")
+            print("-------------------\n")
+    print("==========================================")
+    print("RAM Usage\n")
+    print(f"Total RAM: {ramTotal}GB\nRAM percent used: {ram.percent}%\nRAM used: {ramUsed}GB | RAM free: {ramFree}GB\n")
+    print("==========================================")
     time.sleep(0.5)

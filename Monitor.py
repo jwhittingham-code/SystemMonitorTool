1a349357df433b15666158d2ce4dc264ea98bb53
jwhittingham-code <jwhittingham002@gmail.com>
2025-11-13 13:31:51 +1100

    Refactored code to be reusable when building a TUI or GUI

--- Monitor.py
+++ Monitor.py
@@ -2,33 +2,49 @@ import os
 import psutil
 import time
 
+def updateValues():
+    global cpuCount
+    cpuCount = psutil.cpu_count(logical=True)
+    global cpuPercent
+    cpuPercent = psutil.cpu_percent(interval=0.1, percpu= True)
+    global coretemp
+    coretemp = psutil.sensors_temperatures()['coretemp']
+    #=======================================================================
+    global ram
+    ram = psutil.virtual_memory()
+    global ramTotal
+    ramTotal = round(ram.total/1024/1024/1024,2)
+    global ramUsed
+    ramUsed = round(ram.used/1024/1024/1024,2)
+    global ramFree
+    ramFree = round(ram.available/1024/1024/1024,2) 
+
+def cpuCores():
+    coredict = {}
+    for i in range(cpuCount + 1):
+       core = coretemp[i][0]
+       currenttemp = coretemp[i][1]
+       maxtemp = coretemp[i][2]
+       coreusage = cpuPercent[i-1] 
+       coredict[i] = {"core" : core,"currenttemp": currenttemp,"maxtemp" : maxtemp,"coreusage" : coreusage}
+    return coredict
 
 
 while True:
     
-    cpuCount = psutil.cpu_count(logical=True)
-    cpuPercent = psutil.cpu_percent(interval=0.1, percpu= True)
-    coretemp = psutil.sensors_temperatures()['coretemp']
-    #=======================================================================
-    ram = psutil.virtual_memory()
-    ramTotal = round(ram.total/1024/1024/1024,2)
-    ramUsed = round(ram.used/1024/1024/1024,2)
-    ramFree = round(ram.available/1024/1024/1024,2) 
+    updateValues()
+    coredict = cpuCores()
 
     os.system('clear')
     print("==================")
     print("| System Monitor |")
     print("==================") 
     print("CPU Usage\n")  
-    for i in range(cpuCount+1):
-        core = coretemp[i][0]
-        currenttemp = coretemp[i][1]
-        maxtemp = coretemp[i][2]
-        coreusage = cpuPercent[i-1]
-        if core != 'Package id 0':
-            print(f"{core}:\t {coreusage}%\nCurrent temp: {currenttemp} | Max temp: {maxtemp}\n")
+    for i in coredict:
+        if coredict[i]["core"] != 'Package id 0':
+            print(f"{coredict[i]['core']}:\t {coredict[i]['coreusage']}%\nCurrent temp: {coredict[i]['currenttemp']} | Max temp: {coredict[i]['maxtemp']}\n")
         else:
-            print(f"TotalUsage: {coreusage}%\nCurrent temp: {currenttemp}")
+            print(f"TotalUsage: {coredict[i]['coreusage']}%\nCurrent temp: {coredict[i]['currenttemp']}")
             print("-------------------\n")
     print("==========================================")
     print("RAM Usage\n")
